3575f9e6c91c23439f0c08e9c3ecc80db14a72bc
psakkos <panos.sakkos@cern.ch>
2012-01-12 13:28:27 +0100

    fixed bug in whitelisting

--- whitelist.h
+++ whitelist.h
@@ -3,9 +3,9 @@
 
 #define MAX_LIST_SIZE 16
 
-#define LIST_FULL -1
+#define LIST_FULL 0
 #define WHITELISTED 1
-#define NOT_WHITELISTED -1
+#define NOT_WHITELISTED 0
 
 int whitelist(const char *process_name);
 
