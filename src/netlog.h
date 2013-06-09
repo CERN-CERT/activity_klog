cee0276fe0922de7d274945b5006c7c5e4ca342c
Vincent Brillault <git@lerya.net>
2013-06-09 21:14:23 +0200

    Add missing define

--- src/netlog.h
+++ src/netlog.h
@@ -23,11 +23,17 @@
 
 #define WHITELISTING 1
 
+/* Maximum number of whitelisted element in module parameter */
+#define MAX_WHITELIST_SIZE 100
+
 /* Error codes */
 
 #define CONNECT_PROBE_FAILED 1
 #define ACCEPT_PROBE_FAILED 2
 #define CLOSE_PROBE_FAILED 3
 #define BIND_PROBE_FAILED 4
+
+/* Separator for the whitelisting */
+#define FIELD_SEPARATOR '|'
 
 #endif /* __NETLOG__ */
