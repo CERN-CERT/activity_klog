68efd35279ecf2f6bcb048e62dbe3626579f2216
Vincent Brillault <git@lerya.net>
2013-06-09 21:13:21 +0200

    WIP: Clean inet_utils
    Most of its function were merged and integrated in probes

--- src/inet_utils.h
+++ src/inet_utils.h
@@ -28,26 +28,8 @@ int is_tcp(struct socket *sock);
 
 int is_udp(struct socket *sock);
 
-char *get_destination_ip_sk(const struct sock *sk);
-
-char *get_destination_ip(const struct socket *sock);
-
-int get_source_port(struct socket *sock);
-
-int get_destination_port(struct socket *sock);
-
-char *get_source_ip_sk(const struct sock *sk);
-
-char *get_source_ip(const struct socket *sock);
-
-char *get_ip(const struct sockaddr *addr);
-
-int any_ip_address(const char *ip);
-
-int looks_like_ipv6(const char *ip);
-
 int valid_port_number(const int port);
 
-int looks_like_valid_ip(const char *ip);
+void copy_ip(void *dst, void *src, unsigned short family);
 
 #endif /* __NETLOG_IPUTILS__ */
