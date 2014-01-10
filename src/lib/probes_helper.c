9692dd817642facaf758d94f537c5da4717714ef
Vincent Brillault <git@lerya.net>
2014-01-10 22:12:32 +0100

    lib: abide by the kernel coding rules
    Used linux/scripts/checkpatch.pl to fix warnings/errors
    Except for >80 char comments

--- src/lib/probes_helper.c
+++ src/lib/probes_helper.c
@@ -4,61 +4,65 @@
 #include "sparse_compat.h"
 #include "probes_helper.h"
 
+/* Printing function */
+#undef pr_fmt
+#define pr_fmt(fmt) MODULE_NAME ": " fmt
+
 int handler_fault(struct kprobe *p, struct pt_regs *regs, int trap_number)
 {
-	switch(trap_number)
-	{
-		case SIGABRT:
-		case SIGSEGV:
-		case SIGQUIT:
-			//TODO Other signals that we need to handle?
-			printk(KERN_ERR MODULE_NAME ": fault handler: Detected fault %d from inside probes.", trap_number);
-			return 0;
-		default:
-			return 0;
+	switch (trap_number) {
+	case SIGABRT:
+	case SIGSEGV:
+	case SIGQUIT:
+		/* TODO Other signals that we need to handle? */
+		pr_err(" fault handler: Detected fault %d from inside probes.", trap_number);
+		return 0;
+	default:
+		return 0;
 	}
 }
 
-void unplant_jprobe(struct jprobe * probe) __must_hold(probe_lock)
+void unplant_jprobe(struct jprobe *probe) __must_hold(probe_lock)
 {
-	printk(KERN_INFO MODULE_NAME ":\t[+] Unplanting jprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Unplanting jprobe on %s\n", probe->kp.symbol_name);
 	unregister_jprobe(probe);
-	printk(KERN_INFO MODULE_NAME ":\t[+] Unplanted jprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Unplanted jprobe on %s\n", probe->kp.symbol_name);
 	probe->kp.addr = NULL;
 }
 
-int plant_jprobe(struct jprobe * probe) __must_hold(probe_lock)
+int plant_jprobe(struct jprobe *probe) __must_hold(probe_lock)
 {
 	int err;
 
-	printk(KERN_INFO MODULE_NAME ":\t[+] Planting jprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Planting jprobe on %s\n", probe->kp.symbol_name);
 	err = register_jprobe(probe);
 	if (err < 0)
-		printk(KERN_INFO MODULE_NAME ":\t[-] Failed to planted jprobe on %s: %i\n", probe->kp.symbol_name, err);
+		pr_info("\t[-] Failed to planted jprobe on %s: %i\n", probe->kp.symbol_name, err);
 	else
-		printk(KERN_INFO MODULE_NAME ":\t[+] Planted jprobe on %s\n", probe->kp.symbol_name);
+		pr_info("\t[+] Planted jprobe on %s\n", probe->kp.symbol_name);
 
 	return err;
 }
 
-void unplant_kretprobe(struct kretprobe * probe) __must_hold(probe_lock)
+void unplant_kretprobe(struct kretprobe *probe) __must_hold(probe_lock)
 {
-	printk(KERN_INFO MODULE_NAME ":\t[+] Unplanting kretprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Unplanting kretprobe on %s\n", probe->kp.symbol_name);
 	unregister_kretprobe(probe);
-	printk(KERN_INFO MODULE_NAME ":\t[+] Unplanted kretprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Unplanted kretprobe on %s\n", probe->kp.symbol_name);
 	probe->kp.addr = NULL;
 }
 
-int plant_kretprobe(struct kretprobe * probe) __must_hold(probe_lock)
+int plant_kretprobe(struct kretprobe *probe) __must_hold(probe_lock)
 {
 	int err;
 
-	printk(KERN_INFO MODULE_NAME ":\t[+] Planting kretprobe on %s\n", probe->kp.symbol_name);
+	pr_info("\t[+] Planting kretprobe on %s\n", probe->kp.symbol_name);
 	err = register_kretprobe(probe);
 	if (err < 0)
-		printk(KERN_INFO MODULE_NAME ":\t[-] Failed to planted kretprobe on %s: %i\n", probe->kp.symbol_name, err);
+		pr_info("\t[-] Failed to planted kretprobe on %s: %i\n", probe->kp.symbol_name, err);
 	else
-		printk(KERN_INFO MODULE_NAME ":\t[+] Planted kretprobe on %s\n", probe->kp.symbol_name);
+		pr_info("\t[+] Planted kretprobe on %s\n", probe->kp.symbol_name);
 
 	return err;
 }
+
--- src/lib/probes_helper.h
+++ src/lib/probes_helper.h
@@ -3,8 +3,8 @@
 
 int handler_fault(struct kprobe *p, struct pt_regs *regs, int trap_number);
 
-void unplant_jprobe(struct jprobe * probe) __must_hold(probe_lock);
-int plant_jprobe(struct jprobe * probe) __must_hold(probe_lock);
+void unplant_jprobe(struct jprobe *probe) __must_hold(probe_lock);
+int plant_jprobe(struct jprobe *probe) __must_hold(probe_lock);
 
-void unplant_kretprobe(struct kretprobe * probe) __must_hold(probe_lock);
-int plant_kretprobe(struct kretprobe * probe) __must_hold(probe_lock);
+void unplant_kretprobe(struct kretprobe *probe) __must_hold(probe_lock);
+int plant_kretprobe(struct kretprobe *probe) __must_hold(probe_lock);
